48ceffb1665bb37fe4153be7e3f6978b6c272ccc
richyliu <richy.liu.2002@gmail.com>
2019-03-09 12:39:13 -0800

    start

---
+++ App.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open App.js to start working on your app!</Text>
+        <Text>Open on your app!</Text>
+        <Banas/>
+      </View>
+    );
+  }
+}
+
+
+class Banas extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>"hello"</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
