17d35d757b6faf7347d2007d1a425a81130643a4
richyliu <richy.liu.2002@gmail.com>
2019-03-09 15:47:58 -0800

    add view list

--- src/containers/view-item/ViewList.js
+++ src/containers/view-item/ViewList.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 import { StyleSheet, FlatList, Text, View } from 'react-native';
 
+import { Link } from 'react-router-native';
+
+const items = [
+  { key: 'Devin', name: 'foo' },
+  { key: 'Jackson' },
+  { key: 'James' },
+  { key: 'Joel' },
+  { key: 'John' },
+  { key: 'Jillian' },
+  { key: 'Jimmy' },
+  { key: 'Julie' },
+];
+
 export default class ViewList extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>view list</Text>
+        <FlatList
+          data={items}
+          renderItem={({ item }) => <ListItem item={item} />}
+        />
       </View>
     );
   }
 }
 
+const ListItem = ({ item }) => (
+  <Link to="/about">
+    <Text style={styles.listItem}>{item.name}</Text>
+  </Link>
+);
+
 const styles = StyleSheet.create({
   container: {
     marginTop: 25,
     padding: 10,
+  },
+  listItem: {
+    padding: 10,
+    fontSize: 12,
   },
 });
