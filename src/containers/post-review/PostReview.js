9a9cfa441006cdbfd4ce3d4b78ec25a239a1461a
richyliu <richy.liu.2002@gmail.com>
2019-03-09 18:19:21 -0800

    finish post review

--- src/containers/post-review/PostReview.js
+++ src/containers/post-review/PostReview.js
@@ -1,13 +1,68 @@
-import React, { Component } from "react";
-import { View, Text } from "react-native";
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TextInput, Slider } from 'react-native';
 
 export default class PostReview extends Component {
+  state = {
+    name: '',
+    price: 0,
+    review: 0,
+  };
+
+  updateName = name => {
+    this.setState(state => ({ ...state, name }));
+  };
+  updatePrice = price => {
+    this.setState(state => ({ ...state, price }));
+  };
+  updateReview = review => {
+    this.setState(state => ({ ...state, review }));
+  };
+
   render() {
     const upc = this.props.match.params.upc;
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Hello from Post Review! UPC code is {upc}</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Item name"
+          onChangeText={this.updateName}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Item price"
+          keyboardType="decimal-pad"
+          onChangeText={this.updatePrice}
+        />
+        <Text style={styles.label}>Review: {this.state.review}/5</Text>
+        <Slider
+          style={styles.slider}
+          step={0.5}
+          minimumValue={0}
+          maximumValue={5}
+          value={this.state.age}
+          onValueChange={this.updateReview}
+        />
+        <Text>{this.state.name}</Text>
+        <Text>{this.state.price}</Text>
+        <Text>{this.state.review}</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  input: {
+    marginTop: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+  slider: {},
+  label: {
+    marginTop: 10,
+  }
+});
--- src/containers/view-item/ViewList.js
+++ src/containers/view-item/ViewList.js
@@ -4,28 +4,146 @@ import { StyleSheet, FlatList, Text, View } from 'react-native';
 import { Link } from 'react-router-native';
 
 const items = [
-
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302093, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302094, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302095, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302096, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302097, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302098, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302099, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302100, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302101, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302102, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302103, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302104, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302105, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302106, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302107, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302108, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302109, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302110, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302111, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302112, type: 'foo' },
-  { name: 'blueberries', price: 20, review: 4.5, upc: 2302113, type: 'foo' },
+  {
+    name: 'Fantastic Steel Sausages',
+    price: 35.28105662006827,
+    review: 3.5799123212450423,
+    upc: 13047,
+    type: 'upc',
+  },
+  {
+    name: 'Awesome Metal Keyboard',
+    price: 22.216984129511673,
+    review: 2.989168549079524,
+    upc: 29193,
+    type: 'upc',
+  },
+  {
+    name: 'Refined Granite Keyboard',
+    price: 36.06635360111328,
+    review: 3.4156108879965172,
+    upc: 37258,
+    type: 'upc',
+  },
+  {
+    name: 'Gorgeous Rubber Chicken',
+    price: 25.55289676947745,
+    review: 2.5787440605421352,
+    upc: 129,
+    type: 'upc',
+  },
+  {
+    name: 'Rustic Steel Chair',
+    price: 26.249949444436567,
+    review: 3.556917891164706,
+    upc: 59336,
+    type: 'upc',
+  },
+  {
+    name: 'Handmade Rubber Pants',
+    price: 32.11406405042659,
+    review: 0.8015782523211246,
+    upc: 80617,
+    type: 'upc',
+  },
+  {
+    name: 'Handmade Fresh Bike',
+    price: 35.71537776163686,
+    review: 2.4300452953534712,
+    upc: 4316,
+    type: 'upc',
+  },
+  {
+    name: 'Small Metal Ball',
+    price: 36.48689661582345,
+    review: 3.656099669656667,
+    upc: 35223,
+    type: 'upc',
+  },
+  {
+    name: 'Incredible Metal Car',
+    price: 32.998269269958016,
+    review: 1.4552621328887316,
+    upc: 21478,
+    type: 'upc',
+  },
+  {
+    name: 'Handmade Rubber Gloves',
+    price: 31.825525566396667,
+    review: 4.384111915287933,
+    upc: 38375,
+    type: 'upc',
+  },
+  {
+    name: 'Tasty Cotton Shirt',
+    price: 25.187842251512514,
+    review: 0.8770799932430695,
+    upc: 44561,
+    type: 'upc',
+  },
+  {
+    name: 'Rustic Rubber Sausages',
+    price: 36.21969358439691,
+    review: 4.542433221629425,
+    upc: 36389,
+    type: 'upc',
+  },
+  {
+    name: 'Generic Cotton Towels',
+    price: 29.395789634392745,
+    review: 0.880509634672173,
+    upc: 96096,
+    type: 'upc',
+  },
+  {
+    name: 'Incredible Plastic Pizza',
+    price: 34.37851056550238,
+    review: 4.624841265638428,
+    upc: 25270,
+    type: 'upc',
+  },
+  {
+    name: 'Gorgeous Plastic Bike',
+    price: 31.834030700945895,
+    review: 2.723089076208791,
+    upc: 9071,
+    type: 'upc',
+  },
+  {
+    name: 'Intelligent Steel Chair',
+    price: 23.094517549350535,
+    review: 3.5008350098615972,
+    upc: 55098,
+    type: 'upc',
+  },
+  {
+    name: 'Refined Metal Sausages',
+    price: 32.81735563870018,
+    review: 0.2641005174515909,
+    upc: 61790,
+    type: 'upc',
+  },
+  {
+    name: 'Incredible Soft Tuna',
+    price: 21.13111404532772,
+    review: 4.3868162874244945,
+    upc: 36312,
+    type: 'upc',
+  },
+  {
+    name: 'Rustic Soft Towels',
+    price: 20.98810360573463,
+    review: 0.6019689480526824,
+    upc: 35712,
+    type: 'upc',
+  },
+  {
+    name: 'Incredible Wooden Bike',
+    price: 22.953608934189162,
+    review: 3.3495478841094917,
+    upc: 91764,
+    type: 'upc',
+  },
 ];
 
 export default class ViewList extends React.Component {
@@ -43,11 +161,13 @@ export default class ViewList extends React.Component {
 }
 
 const ListItem = ({ item }) => (
-  <Link to={`/view-item/:${item.upc}/:${item.type}`} underlayColor="#f0f4f7">
+  <Link to={`/view-item/${item.upc}/${item.type}`} underlayColor="#f0f4f7">
     <View style={styles.listItem}>
       <Text style={styles.itemName}>{item.name}</Text>
       <Text style={styles.itemPrice}>${item.price.toFixed(2)}</Text>
-      <Text style={styles.itemReview}>{item.review}/5</Text>
+      <Text style={styles.itemReview}>
+        {Math.floor(item.review * 100) / 100}/5
+      </Text>
     </View>
   </Link>
 );
