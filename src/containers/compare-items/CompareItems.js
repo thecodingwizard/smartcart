c61e375c46fa3f93866654e18bb5b4948544023b
richyliu <richy.liu.2002@gmail.com>
2019-03-10 04:22:15 -0700

    finish compare items

--- src/containers/compare-items/CompareItems.js
+++ src/containers/compare-items/CompareItems.js
@@ -1,73 +1,99 @@
-import React, { Component } from "react"
-import * as FireBase from "firebase/app"
-import "firebase/firestore"
-import {FlatList, StyleSheet, Text, View} from "react-native";
+import React, { Component } from 'react';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import { FlatList, StyleSheet, Text, View, ScrollView } from 'react-native';
+
+const products = firebase.firestore().collection('products');
 
 export default class CompareItems extends Component {
-  constructor(props) {
-    super(props);
-    this.state={
-      a: {},
-      b: {},
-    }
-  }
+  static navigationOptions = {
+    title: 'Compare Items',
+  };
+
+  state = {
+    a: [],
+    aName: '',
+    b: [],
+    bName: '',
+  };
+
   render() {
-    // firebase.firestore().collection("products").doc("00096362").get().then(function(res) {
-    //   var data = res.data();
-    //
-    // })
-    return (
-      <NutritionFacts/>
-    )
+    if (
+      this.state.a &&
+      this.state.a.length > 0 &&
+      this.state.b &&
+      this.state.b.length > 0
+    ) {
+      return (
+        <ScrollView style={style.comparion}>
+          <NutritionFacts nutritions={this.state.a} name={this.state.aName} />
+          <NutritionFacts nutritions={this.state.b} name={this.state.bName} />
+        </ScrollView>
+      );
+    } else {
+      if (this.state.a.length == 0) {
+        products
+          .doc('00502627')
+          .get()
+          .then(res => {
+            var data = res.data();
+            this.setState(state => ({
+              ...state,
+              a: data.nutritions,
+              aName: data.name,
+            }));
+          });
+      }
+      if (this.state.b.length == 0) {
+        products
+          .doc('00502627')
+          .get()
+          .then(res => {
+            var data = res.data();
+            this.setState(state => ({
+              ...state,
+              b: data.nutritions,
+              bName: data.name,
+            }));
+          });
+      }
+      return (
+        <View>
+          <Text>Loading...</Text>
+        </View>
+      );
+    }
   }
 }
 
-const nutritions = [
-  { name: 'Calories', amount: '100', percent: '5', indented: false },
-  { name: 'Total Fat', amount: '0.5g', percent: '1', indented: false },
-  { name: 'Saturated Fat', amount: '0g', percent: '0', indented: true },
-  { name: 'Trans Fat', amount: '0g', percent: '0', indented: true },
-  { name: 'Cholesterol', amount: '0mg', percent: '0', indented: false },
-  { name: 'Sodium', amount: '380mg', percent: '16', indented: false },
-  { name: 'Total Carbohydrate', amount: '22g', percent: '7', indented: false },
-  { name: 'Dietary Fiber', amount: '6g', percent: '24', indented: true },
-  { name: 'Sugars', amount: '1g', percent: '', indented: true },
-  { name: 'Protein', amount: '6g', percent: '', indented: false },
-];
-
 const renderItem = ({ item }) => (
-    <View style={style.nutritionFactsItem}>
-      <Text
-          style={{
-            flex: 3,
-            fontWeight: '900',
-            ...(item.indented ? style.nutritionFactsUnbold : {}),
-          }}
-      >
-        {(item.indented ? '        ' : '') + item.name}
-      </Text>
-      <Text style={{ flex: 1 }}>{item.amount}</Text>
-      <Text style={{ flex: 1 }}>
-        {item.percent.length > 0 && item.percent + '%'}
-      </Text>
-    </View>
+  <View style={style.nutritionFactsItem}>
+    <Text
+      style={{
+        flex: 3,
+        fontWeight: '900',
+        ...(item.indented ? style.nutritionFactsUnbold : {}),
+      }}
+    >
+      {(item.indented ? '        ' : '') + item.name}
+    </Text>
+    <Text style={{ flex: 1 }}>{item.amount}</Text>
+  </View>
 );
 
-const NutritionFacts = () => (
-    <View style={style.nutritionFacts}>
-      <Text style={style.nutritionFactsTitle}>Nutrition Facts</Text>
-      <FlatList
-          data={[
-            { name: 'Category', amount: 'Amount', percent: 'Daily ' },
-            ...nutritions,
-          ]}
-          renderItem={renderItem}
-          keyExtractor={({ name }) => name}
-      />
-    </View>
+const NutritionFacts = ({ nutritions, name }) => (
+  <View style={style.nutritionFacts}>
+    <Text style={style.nutritionFactsTitle}>{name}</Text>
+    <FlatList
+      data={[{ name: 'Category', amount: 'Amount' }, ...nutritions]}
+      renderItem={renderItem}
+      keyExtractor={({ name }) => name}
+    />
+  </View>
 );
 
 const style = StyleSheet.create({
+  comparison: {},
   nutritionFacts: {
     margin: 10,
   },
