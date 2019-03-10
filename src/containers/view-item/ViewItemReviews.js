47fbea81f8d03d372dc64b26c781fa67c9cfeb2e
Nathan Wang <nathan.r.wang@gmail.com>
2019-03-10 04:49:53 -0700

    gl richard

--- src/containers/view-item/ViewItemReviews.js
+++ src/containers/view-item/ViewItemReviews.js
@@ -1,15 +1,37 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Text } from "react-native-elements";
+import { getItemReviews } from "../../actions";
 
 class ViewItemReviews extends Component {
   static navigationOptions = {
     title: "Product Reviews",
   };
 
+  componentDidMount() {
+    const upc = this.props.navigation.getParam('upc');
+    this.props.dispatch(getItemReviews(upc));
+  }
+
+  componentDidUpdate(prevProps) {
+    if (
+      this.props.navigation.getParam('upc') !==
+      prevProps.navigation.getParam('upc')
+    ) {
+      this.props.dispatch(
+        getItemReviews(this.props.navigation.getParam('upc'))
+      );
+    }
+  }
+
   render() {
     return (
-      <Text>Item Review, Todo</Text>
+      <Text>
+        UPC Code: {this.props.navigation.getParam("upc")}
+        Reviews: {JSON.stringify(this.props.reviews)}
+        {this.props.loading && "Loading"}
+        {this.props.error && "Error: " + this.props.error}
+      </Text>
     )
   }
 }
@@ -17,6 +39,7 @@ class ViewItemReviews extends Component {
 const mapStateToProps = state => ({
   loading: state.props.loading,
   error: state.props.error,
+  reviews: state.props.reviews,
 });
 
 export default connect(mapStateToProps)(ViewItemReviews);
