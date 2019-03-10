2f9328351a824a84d5e6af1ae75326b5ccd7507e
richyliu <richy.liu.2002@gmail.com>
2019-03-09 20:00:32 -0800

    add validation to adding review

--- src/containers/post-review/PostReview.js
+++ src/containers/post-review/PostReview.js
@@ -12,8 +12,10 @@ import { Rating, AirbnbRating } from 'react-native-elements';
 export default class PostReview extends Component {
   state = {
     name: '',
-    price: 0,
-    review: 0,
+    price: '',
+    review: 4.3,
+    nameError: false,
+    priceError: false,
   };
 
   static navigationOptions = {
@@ -31,8 +33,35 @@ export default class PostReview extends Component {
   };
 
   addReview = () => {
+    if (this.state.name.length <= 0) {
+      this.setState(state => ({ ...state, nameError: true }));
+      return;
+    } else {
+      this.setState(state => ({ ...state, nameError: false }));
+    }
+    if (this.state.price.length <= 0) {
+      this.setState(state => ({ ...state, priceError: true }));
+      return;
+    } else {
+      this.setState(state => ({ ...state, priceError: false }));
+    }
+
+    // check that price is valid
+    const price = Number(this.state.price);
+    if (price === NaN) {
+      this.setState(state => ({ ...state, priceError: true }));
+      return;
+    } else {
+      this.setState(state => ({ ...state, priceError: false }));
+    }
+
+    if (this.state.priceError || this.state.nameError) return;
+
     // add review to firebase
-    alert(`Added review of ${this.state.name} with price of $${this.state.price} and a review of ${this.state.review}/5`)
+    alert(
+      `Added review of ${this.state.name} with price ` +
+        `of $${price} and a review of ${this.state.review}/5`
+    );
 
     // navigate back to home
     this.props.navigation.navigate('Home');
@@ -44,12 +73,18 @@ export default class PostReview extends Component {
       <View style={styles.container}>
         <Text style={styles.title}>UPC code: {upc}</Text>
         <TextInput
-          style={styles.input}
+          style={{
+            ...styles.input,
+            ...(this.state.nameError ? styles.inputError : {}),
+          }}
           placeholder="Item name"
           onChangeText={this.updateName}
         />
         <TextInput
-          style={styles.input}
+          style={{
+            ...styles.input,
+            ...(this.state.priceError ? styles.inputError : {}),
+          }}
           placeholder="Item price"
           keyboardType="decimal-pad"
           onChangeText={this.updatePrice}
@@ -57,7 +92,7 @@ export default class PostReview extends Component {
         <Rating
           showRating
           fractions={1}
-          startingValue={4.3}
+          startingValue={this.state.review}
           onFinishRating={this.updateReview}
         />
         <Button title="Add" onPress={this.addReview} />
@@ -78,6 +113,9 @@ const styles = StyleSheet.create({
     padding: 10,
     borderWidth: 1,
     borderRadius: 10,
+  },
+  inputError: {
+    borderColor: 'red',
   },
   label: {
     marginTop: 10,
