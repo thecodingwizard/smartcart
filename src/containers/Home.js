932475f0b60bc8e2aca5af2d7a3acb753f7f96d8
richyliu <richy.liu.2002@gmail.com>
2019-03-09 23:47:46 -0800

    add icons to home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,48 +1,89 @@
 import React, { Component } from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  TouchableHighlight,
+  ScrollView,
+} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     marginTop: 25,
     padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageWrapper: {
+    marginTop: 50,
+    alignItems: 'center',
+    color: 'blue',
+  },
+  image: {
+    width: 150,
+    height: 150,
   },
 });
 
 export default class Home extends Component {
   static navigationOptions = {
-    title: "Smart Cart",
+    title: 'Smart Cart',
     headerStyle: {
-      backgroundColor: "#0967D2",
+      backgroundColor: '#0967D2',
     },
     headerTintColor: '#fff',
   };
 
   render() {
     return (
-      <View style={styles.container}>
-        <Button
-          title="Go to View Item"
+      <ScrollView contentContainerStyle={styles.container}>
+        <TouchableHighlight
+          underlayColor="#f0f4f7"
           onPress={() =>
             this.props.navigation.navigate('ViewItem', {
               upc: '00430999',
             })
           }
-        />
-        <Button
-          title="Go to Post Review"
+        >
+          <View style={styles.imageWrapper}>
+            <Image
+              style={styles.image}
+              source={require('../../assets/search.png')}
+            />
+            <Text>View item</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight
+          underlayColor="#f0f4f7"
           onPress={() =>
             this.props.navigation.navigate('PostReview', {
               upc: '00430999',
             })
           }
-        />
-        <Button
-          title="Go to Scan Item"
-          onPress={() =>
-            this.props.navigation.navigate('ScanItem')
-          }
-        />
-      </View>
+        >
+          <View style={styles.imageWrapper}>
+            <Image
+              style={styles.image}
+              source={require('../../assets/pencil-alt.png')}
+            />
+            <Text>Post review</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight
+          underlayColor="#f0f4f7"
+          onPress={() => this.props.navigation.navigate('ScanItem')}
+        >
+          <View style={styles.imageWrapper}>
+            <Image
+              style={styles.image}
+              source={require('../../assets/scanner-keyboard.png')}
+            />
+            <Text>Scan item</Text>
+          </View>
+        </TouchableHighlight>
+      </ScrollView>
     );
   }
 }
