2afcaa6cfb99af9ae9d9846ad2d37a639e0a4c3b
slopey112 <howard_peng@icloud.com>
2019-03-09 19:10:37 -0800

    add registration page

---
+++ src/auth/RegisterMod.js
@@ -0,0 +1,114 @@
+import React, {Component} from "react";
+import {StyleSheet, TextInput, View, Button, Text} from "react-native";
+
+export default class RegisterMod extends Component {
+  render() {
+    return (
+        <View>
+          <Register/>
+        </View>
+    );
+  }
+}
+
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      firstName: "",
+      lastName: "",
+      userName: "",
+      password: "",
+      confirmPassword: "",
+      wrongPassword: false,
+      usernameTaken: false,
+      emailTaken: false
+    };
+  }
+
+  register = () => {
+    this.setState(state => ({...state, wrongPassword: false}));
+    this.setState(state => ({...state, usernameTaken: false}));
+    this.setState(state => ({...state, emailTaken: false}));
+    if (this.state.password !== this.state.confirmPassword) {
+      this.setState(state => ({...state, wrongPassword: true}));
+    } else if (false) {
+      // Place condition for username taken in place of 'false'
+      this.setState(state => ({...state, usernameTaken: true}));
+    } else if (false) {
+      // Place condition for email taken in place of 'false'
+      this.setState(state => ({...state, emailTaken: true}));
+    } else {
+      alert("Signed in!");
+    }
+  };
+
+  render() {
+    return (
+        <View style={style.container}>
+          <TextInput
+              style={style.textBoxStyle}
+              placeholder="First Name"
+              onChangeText={text => this.setState(state => ({...state, firstName: text}))}
+          />
+          <TextInput
+              style={style.textBoxStyle}
+              placeholder="Last Name"
+              onChangeText={text => this.setState(state => ({...state, lastName: text}))}
+          />
+          <TextInput
+              style={style.textBoxStyle}
+              placeholder="Email"
+              onChangeText={text => this.setState(state => ({...state, email: text}))}
+          />
+          <TextInput
+              style={style.textBoxStyle}
+              placeholder="Username"
+              onChangeText={text => this.setState(state => ({...state, userName: text}))}
+          />
+          <TextInput
+              style={style.textBoxStyle}
+              placeholder="Password"
+              onChangeText={text => this.setState(state => ({...state, password: text}))}
+          />
+          <TextInput
+              style={style.textBoxStyle}
+              placeholder="Confirm Password"
+              onChangeText={text => this.setState(state => ({...state, confirmPassword: text}))}
+          />
+          <View style={style.buttonStyle}>
+            <Button
+                onPress={this.register}
+                title="Register"
+            />
+          </View>
+          {(this.state.wrongPassword) &&
+          <Text style={style.textStyle}>Sorry, passwords do not match.</Text>}
+          {(this.state.usernameTaken) &&
+          <Text style={style.textStyle}>Sorry, this username is taken.</Text>}
+          {(this.state.emailTaken) &&
+          <Text style={style.textStyle}>Sorry, this email has been taken.</Text>}
+        </View>
+    )
+  }
+}
+
+const style = StyleSheet.create({
+  container: {
+    height: 2000,
+    padding: 10,
+    backgroundColor: "white"
+  }, textBoxStyle: {
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 6,
+    padding: 5,
+    margin: 5
+  }, buttonStyle: {
+    height: 40,
+    width: 350
+  }, textStyle: {
+    color: "black"
+  }
+});
